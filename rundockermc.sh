1410f940c4d9042e1fc318cbe6795e70f50aed43
root <root@docker.local>
2019-11-19 00:06:16 +0000

    fixed docker build script

--- rundockermc.sh
+++ rundockermc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker build ./bedrock
+docker build ./bedrock -t bedrock
 docker run -d --name=minecraft\
     -v '/minecraft/worlds:/bedrock-server/worlds'\
     -v '/minecraft/server.properties:/bedrock-server/server.properties'\
