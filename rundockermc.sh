f50f6447cb429fd807d38e2a7f430158743778fc
root <root@minecraft.local>
2019-12-14 18:36:50 +0000

    updated to 1.14

--- rundockermc.sh
+++ rundockermc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 docker build ./bedrock -t bedrock
 docker run -d --name=minecraft\
-    -v '/minecraft/worlds:/bedrock-server/worlds'\
-    -v '/minecraft/server.properties:/bedrock-server/server.properties'\
+    -v '/dockerdata/minecraft/worlds:/bedrock-server/worlds'\
+    -v '/dockerdata/minecraft/server.properties:/bedrock-server/server.properties'\
     --network host\
     --restart=always\
     bedrock
